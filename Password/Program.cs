58e26f571f3f7f7d777f07a9c658c3d93ec1ae34
Feaxmutch <feaxmutch@gmail.com>
2024-10-06 12:05:25 +0300

    Implemented password check and message showing

--- Password/Program.cs
+++ Password/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int attemps = 3;
+            string password = "123word";
+            string message = "Пончик";
+
+            for (; attemps > 0; attemps--)
+            {
+                Console.Clear();
+
+                Console.WriteLine($"Осталось {attemps} попыток");
+                Console.Write("Введите пароль: ");
+                string userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine(message);
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Неверный пароль.");
+                    Console.ReadKey();
+                }
+            }
         }
     }
 }
